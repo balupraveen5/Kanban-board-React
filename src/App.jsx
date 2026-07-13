a13e7bede3156b7a6a1c92531bacf12b1b92221e
Balu <Balu@gmail.com>
2026-07-13 16:42:55 +0530

    Lifting up state to App

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,39 @@ import Board from './components/Board';
 
 function App() {
   
+  const[cards,setCards]=useState([
+        {
+      id: crypto.randomUUID(),
+      title: "Learn React",
+      description: "Finish React basics",
+      column: "todo",
+    },
+    {
+      id: crypto.randomUUID(),
+      title: "Read Hooks",
+      description: "Study useState",
+      column: "todo",
+    },
+    {
+      id: crypto.randomUUID(),
+      title: "Build Kanban",
+      description: "Create project",
+      column: "doing",
+    },
+    {
+      id: crypto.randomUUID(),
+      title: "Install Vite",
+      description: "Setup project",
+      column: "done",
+    },
+
+  ])
 
   return (
     <>
      <h1>Kanban-board</h1>
      <Header />
-     <Board />
+     <Board cards={cards} />
     </>
   )
 }
